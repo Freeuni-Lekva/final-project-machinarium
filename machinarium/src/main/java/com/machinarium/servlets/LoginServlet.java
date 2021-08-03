ef2c82a7282d36fd461eb35fc30619e4a967a874
zuka011 <zmuji18@freeuni.edu.ge>
2021-08-03 20:08:52 +0400

    Add Login Servlet implementation
    Related to #11

--- machinarium/src/main/java/com/machinarium/servlets/LoginServlet.java
+++ machinarium/src/main/java/com/machinarium/servlets/LoginServlet.java
@@ -1,27 +1,42 @@
 package com.machinarium.servlets;
 
-import java.io.*;
+import com.machinarium.dao.EncryptedPassword;
+import com.machinarium.dao.User;
+import com.machinarium.dao.UserDAO;
+
 import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-@WebServlet(name = "loginServlet", value = "/login-servlet")
-public class HelloServlet extends HttpServlet {
+import static com.machinarium.servlets.RequestConstants.PARAMETER_PASSWORD;
+import static com.machinarium.servlets.RequestConstants.PARAMETER_USER_NAME;
 
-	private static final 
+@WebServlet(name = "LoginServlet", value = "/LoginServlet")
+public class LoginServlet extends HttpServlet {
 
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
 		ServletContext contextListener = request.getServletContext();
 
-		String userName = request.getParameter("user_name");
-		String password = request.getParameter("password");
+		String userName = request.getParameter(PARAMETER_USER_NAME);
+		String password = request.getParameter(PARAMETER_PASSWORD);
 
+		UserDAO userDao = (UserDAO) contextListener.getAttribute(Listener.ATTRIBUTE_USER_DAO);
 
-		AccountManager accountManager = (AccountManager) contextListener.getAttribute("account_manager");
-
-		if(accountManager.isCorrectPassword(userName, password)) request.getRequestDispatcher("/welcome.jsp").forward(request, response);
-		else response.sendRedirect("incorrect-information.jsp");
+		if (userName == null) {
+			response.sendError(response.SC_UNAUTHORIZED, "The user name must be specified to authenticate.");
+		}
+		else if(password == null) {
+			response.sendError(response.SC_UNAUTHORIZED, "The password must be specified to authenticate.");
+		}
+		else if(!userDao.getUser(userName).getPassword().equals(EncryptedPassword.of(password))) {
+			response.sendError(response.SC_UNAUTHORIZED, "The specified user name or password is incorrect.");
+		} else {
+			response.setStatus(response.SC_SEE_OTHER);
+		}
 	}
+}
---
+++ machinarium/src/main/java/com/machinarium/servlets/RequestConstants.java
@@ -0,0 +1,11 @@
+package com.machinarium.servlets;
+
+public class RequestConstants {
+
+    public static final String PARAMETER_USER_NAME = "user_name";
+    public static final String PARAMETER_PASSWORD = "password";
+    public static final String PARAMETER_EMAIL = "email";
+
+    /* This class is meant for holding constants. */
+    private RequestConstants() {}
+}
