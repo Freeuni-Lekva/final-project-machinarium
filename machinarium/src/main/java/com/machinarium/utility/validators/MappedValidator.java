7485312dd4b4d943052feda0e6f22872faaeaf89
zuka011 <zmuji18@freeuni.edu.ge>
2021-08-04 10:25:47 +0400

    Add comments for MappedValidator
    Related to #11

--- machinarium/src/main/java/com/machinarium/utility/validators/MappedValidator.java
+++ machinarium/src/main/java/com/machinarium/utility/validators/MappedValidator.java
@@ -1,12 +1,29 @@
 package com.machinarium.utility.validators;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
 
 public class MappedValidator implements Validator {
 
+    /**
+     * <p>
+     * Constructs a {@link MappedValidator} from the specified map. The map <b>is not
+     * copied and only the reference is stored.</b>
+     * </p>
+     *
+     * <p>
+     * The specified validator map should be a mapping of the form: <br>
+     *      {Regular Expression String : Condition that the regular expression checks}
+     * </p>
+     *
+     * <p>
+     * The specified inputs will later be checked to see if they <b>fully match</b> the
+     * regular expressions specified in this map. The invalidation reason will be
+     * the one mapped to the first regular expression that doesn't match.
+     * </p>
+     *
+     * @param validationMap The validator map to be used for validations.
+     */
     public MappedValidator(Map<String, String> validationMap) {
         this.validationMap = validationMap;
     }
@@ -20,5 +37,5 @@ public class MappedValidator implements Validator {
         return null;
     };
 
-    private Map<String, String> validationMap;
+    private final Map<String, String> validationMap;
 }
