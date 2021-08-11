e07aca5ff98cfd0f985950172c0a0dabf1cf1156
CSopiko <skurd18@freeuni.edu.ge>
2021-08-11 15:01:07 +0400

    Add sql script creating database tables
    Resolves #18

---
+++ machinarium/src/main/java/com/machinarium/scripts/create_database_tables.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE machinarium_database;
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users{
+	id INT,
+    user_name,
+    user_password,
+    mail
+};
+
+CREATE TABLE item_categories{
+	id INT,
+    
+};
