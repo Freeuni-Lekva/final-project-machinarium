f4ef45429033ddef18ad579ee397c55678e04826
zmuji18 <zmuji18@freeuni.edu.ge>
2021-08-14 18:14:32 +0400

    Add login and registration webpage drafts
    Related to #36

---
+++ machinarium/src/main/webapp/register/register.js
@@ -0,0 +1,31 @@
+var registerButton = document.getElementById("register_button");
+
+registerButton.onclick = function() {
+    var username = document.getElementById("usrn");
+    var password = document.getElementById("psw");
+    var email = document.getElementById("email");
+    var errorMessage = document.getElementById("error_message");
+
+    var request = new XMLHttpRequest();
+    request.open("POST", "/RegisterServlet");
+
+    var data = {    
+        user_name: username.value,
+        password: password.value,
+        email: email.value
+    };
+
+    request.onload = () => {
+        var response = request.response;
+
+        console.assert(request.status !== 400, "A malformed request was sent.");
+
+        if(request.status === 409) {
+            errorMessage.innerHTML = JSON.parse(response).message;
+        } else if(request.status === 201) {
+            window.location.href = "https://www.youtube.com/watch?v=TwTj0226zm4";
+        }
+    }
+
+    request.send(JSON.stringify(data));
+}
