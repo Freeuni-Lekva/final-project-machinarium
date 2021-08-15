5eeab8aba7fb885193dff77a23915fb96b6440e3
ზუკა მუჯირიშვილი <66688335+zuka011@users.noreply.github.com>
2021-08-16 03:53:19 +0400

    Add GameServlet implementation (#76)
    Closes #75

--- machinarium/src/main/java/com/machinarium/dao/ItemDAO.java
+++ machinarium/src/main/java/com/machinarium/dao/ItemDAO.java
@@ -3,7 +3,16 @@ package com.machinarium.dao;
 import com.machinarium.model.Item.Item;
 import com.machinarium.utility.common.ID;
 
+import java.util.List;
+
 public interface ItemDAO {
+
 	Item getItem(ID itemID);
 
+	/**
+	 * Returns a list of all available items from the database.
+	 *
+	 * @return The list of items as a {@link List<Item>} object.
+	 */
+	List<Item> getAllItems();
 }
