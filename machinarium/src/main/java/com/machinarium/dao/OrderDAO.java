90cc895cbc3066d20ffc390c3f790457a8e83749
CSopiko <skurd18@freeuni.edu.ge>
2021-08-15 16:11:33 +0400

    Updat OrderDAO interface and class

--- machinarium/src/main/java/com/machinarium/dao/OrderDAO.java
+++ machinarium/src/main/java/com/machinarium/dao/OrderDAO.java
@@ -1,17 +1,44 @@
+
 package com.machinarium.dao;
 
-import com.machinarium.model.Item.Item;
-import com.machinarium.utility.common.ID;
 import com.machinarium.model.history.Order;
+import com.machinarium.utility.common.ID;
+
 import java.util.List;
 import java.util.Map;
 
 public interface OrderDAO {
-	Order getOrder(String userName, ID orderID);
+	String ORDER_ACTIVE = "active";
+	String ORDER_INACTIVE = "inactive";
+
+	Order getOrder(ID orderID);
+
 	List<Order> getAllOrders(String userName);
 
-	ID addOrder(String userName, Map<ID, Integer> userGives,
-								 Map<ID, Integer> userTakes); // returns ID
-	boolean removeOrder(String userName, ID orderID);
+	ID addOrder(String userName, Map<ID, Integer> userGives, Map<ID, Integer> userTakes); // returns ID
 
+	/**
+	 * Updates the status of the specified order.
+	 *
+	 * @param orderID The {@link ID} of the order.
+	 * @param status The new status as a {@link String}.
+	 * @return True if the update was successful.
+	 */
+	boolean updateOrderStatus(ID orderID, String status);
+
+	/**
+	 * Cancels all orders of the speciifed user.
+	 *
+	 * @param userName The username of the user.
+	 * @return True if the update was successful.
+	 */
+	boolean cancelAllOrders(String userName);
+
+	/**
+	 * Cancels the specified order.
+	 *
+	 * @param orderID The id of the order.
+	 * @return True if the order was successfully canceled.
+	 */
+	boolean cancelOrder(ID orderID);
 }
