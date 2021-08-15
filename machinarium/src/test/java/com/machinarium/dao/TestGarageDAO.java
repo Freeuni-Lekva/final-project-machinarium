67c168d9cbb8d7b8aed92fdbda01263b2f94fce7
Luka Gelashvili <lgela18@freeuni.edu.ge>
2021-08-16 01:03:32 +0400

    ToDo implement TestGarageDAO.

--- machinarium/src/test/java/com/machinarium/dao/TestGarageDAO.java
+++ machinarium/src/test/java/com/machinarium/dao/TestGarageDAO.java
@@ -1,61 +1,89 @@
 package com.machinarium.dao;
 
+import com.machinarium.common.TestDBManager;
+import com.machinarium.common.TestData;
 import com.machinarium.dao.implementation.BlockingConnectionPool;
 import com.machinarium.dao.implementation.GarageDAOClass;
+import com.machinarium.dao.implementation.UserDAOClass;
 import com.machinarium.utility.common.ID;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.io.File;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestGarageDAO {
-	GarageDAO garageDAO;
+	private static ConnectionPool connectionPool;
+	private static UserDAO userDAO;
+
+	@BeforeAll
+	public static void begin() {
+		TestDBManager.resetDB();
+	}
 
 	@BeforeEach
-	public void init() {
-		ConnectionPool connectionPool = BlockingConnectionPool.getInstance();
-		garageDAO = new GarageDAOClass(connectionPool);
+	public void setup() {
+		connectionPool = BlockingConnectionPool.getInstance();
+		userDAO = new UserDAOClass(connectionPool);
 	}
+
+	@AfterEach
+	public void tearDown() {
+		TestDBManager.resetDB();
+	}
+
+	@AfterAll
+	public static void end() {
+		try {
+			connectionPool.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 
 	@Test
-	public void Test_0() {
-
+	public void test() {
+		assertTrue(1 == 1);
 	}
 
-	@Test
-	public void Test_1() {
-
-
-		assertFalse(garageDAO.hasCar("luka"));
-		assertEquals(0, garageDAO.getCarCount("luka"));
-
-		ID carAiD = garageDAO.addEmptyCar("luka", "carA");
-		ID carBiD = garageDAO.addEmptyCar("luka", "carB");
-		ID carCiD = garageDAO.addEmptyCar("luka", "carC");
-		assertEquals(3, garageDAO.getCarCount("luka"));
-		assertEquals(3, garageDAO.getAllCars("luka").size());
-		System.out.println("******" + garageDAO.getAllCars("luka"));
-
-		assertTrue(garageDAO.removeCar(carBiD));
-		assertEquals(2, garageDAO.getCarCount("luka"));
-		assertEquals(2, garageDAO.getAllCars("luka").size());
-
-		assertTrue(garageDAO.removeCar(carCiD));
-		assertEquals(1, garageDAO.getCarCount("luka"));
-		assertEquals(1, garageDAO.getAllCars("luka").size());
-
-		assertTrue(garageDAO.removeCar(carAiD));
-		assertEquals(0, garageDAO.getCarCount("luka"));
-		assertEquals(0, garageDAO.getAllCars("luka").size());
 
 
 
+//	@Test
+//	public void hasCar() {
+//		ID carAiD = garageDAO.addEmptyCar("luka", "carA");
+//		assertEquals(1, garageDAO.getCarCount("luka"));
+//		assertEquals(1, garageDAO.getAllCars("luka").size());
+//
+//		assertTrue(garageDAO.removeCar(carAiD));
+//	}
 
-
-
-
-
-
-	}
+//	@Test
+//	public void Test_1() {
+//		assertFalse(garageDAO.hasCar("luka"));
+//		assertEquals(0, garageDAO.getCarCount("luka"));
+//
+//		ID carAiD = garageDAO.addEmptyCar("luka", "carA");
+//		ID carBiD = garageDAO.addEmptyCar("luka", "carB");
+//		ID carCiD = garageDAO.addEmptyCar("luka", "carC");
+//		assertEquals(3, garageDAO.getCarCount("luka"));
+//		assertEquals(3, garageDAO.getAllCars("luka").size());
+////		System.out.println("******" + garageDAO.getAllCars("luka"));
+//
+//		assertTrue(garageDAO.removeCar(carBiD));
+//		assertEquals(2, garageDAO.getCarCount("luka"));
+//		assertEquals(2, garageDAO.getAllCars("luka").size());
+//
+//		assertTrue(garageDAO.removeCar(carCiD));
+//		assertEquals(1, garageDAO.getCarCount("luka"));
+//		assertEquals(1, garageDAO.getAllCars("luka").size());
+//
+//		assertTrue(garageDAO.removeCar(carAiD));
+//		assertEquals(0, garageDAO.getCarCount("luka"));
+//		assertEquals(0, garageDAO.getAllCars("luka").size());
+//	}
 
 
 }
