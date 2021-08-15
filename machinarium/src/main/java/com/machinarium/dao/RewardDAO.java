f153ae834fc423f3e231ba233edc24d794b175d2
ზუკა მუჯირიშვილი <66688335+zuka011@users.noreply.github.com>
2021-08-16 02:20:51 +0400

    Update machinarium/src/main/java/com/machinarium/dao/RewardDAO.java

--- machinarium/src/main/java/com/machinarium/dao/RewardDAO.java
+++ machinarium/src/main/java/com/machinarium/dao/RewardDAO.java
@@ -11,5 +11,12 @@ public interface RewardDAO {
 	Reward getReward(ID rewardID);
 
 	List<Reward> getRewards(String userName);
-
+       /**
+	 * Adds a reward to the specified user.
+	 *
+	 * @param userName The username of the receiver of the reward.
+	 * @param rewards The contents of the reward as a mapping of {@link Item} {@link ID}s to amounts.
+	 * @return True if the reward was was successfully added.
+	 */
+	boolean addReward(String userName, Map<ID, Integer> rewards);
 }
