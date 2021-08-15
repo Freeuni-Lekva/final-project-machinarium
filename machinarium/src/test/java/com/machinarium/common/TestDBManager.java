35f104a6504c1a890def33814fd9942babb93e46
Luka Gelashvili <lgela18@freeuni.edu.ge>
2021-08-16 02:51:42 +0400

    Zuka suggestion approved.

--- machinarium/src/test/java/com/machinarium/common/TestDBManager.java
+++ machinarium/src/test/java/com/machinarium/common/TestDBManager.java
@@ -9,6 +9,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TestDBManager {
 
@@ -20,24 +21,14 @@ public class TestDBManager {
 			Statement getUserStatement = con.createStatement();
 
 			List<String> allQueries = new ArrayList<>();
-			allQueries.addAll(Arrays.asList("SET FOREIGN_KEY_CHECKS = 0; ", "TRUNCATE TABLE user_order; ", "SET FOREIGN_KEY_CHECKS = 1; "));
-			allQueries.addAll(Arrays.asList("SET FOREIGN_KEY_CHECKS = 0; ", "TRUNCATE TABLE order_item; ", "SET FOREIGN_KEY_CHECKS = 1; "));
-			allQueries.addAll(Arrays.asList("SET FOREIGN_KEY_CHECKS = 0; ", "TRUNCATE TABLE orders; ", "SET FOREIGN_KEY_CHECKS = 1; "));
-			allQueries.addAll(Arrays.asList("SET FOREIGN_KEY_CHECKS = 0; ", "TRUNCATE TABLE user_game; ", "SET FOREIGN_KEY_CHECKS = 1; "));
-			allQueries.addAll(Arrays.asList("SET FOREIGN_KEY_CHECKS = 0; ", "TRUNCATE TABLE game_results; ", "SET FOREIGN_KEY_CHECKS = 1; "));
-			allQueries.addAll(Arrays.asList("SET FOREIGN_KEY_CHECKS = 0; ", "TRUNCATE TABLE reward_item; ", "SET FOREIGN_KEY_CHECKS = 1; "));
-			allQueries.addAll(Arrays.asList("SET FOREIGN_KEY_CHECKS = 0; ", "TRUNCATE TABLE rewards; ", "SET FOREIGN_KEY_CHECKS = 1; "));
-			allQueries.addAll(Arrays.asList("SET FOREIGN_KEY_CHECKS = 0; ", "TRUNCATE TABLE user_statistics; ", "SET FOREIGN_KEY_CHECKS = 1; "));
-			allQueries.addAll(Arrays.asList("SET FOREIGN_KEY_CHECKS = 0; ", "TRUNCATE TABLE garage_item; ", "SET FOREIGN_KEY_CHECKS = 1; "));
-			allQueries.addAll(Arrays.asList("SET FOREIGN_KEY_CHECKS = 0; ", "TRUNCATE TABLE garage_car; ", "SET FOREIGN_KEY_CHECKS = 1; "));
-			allQueries.addAll(Arrays.asList("SET FOREIGN_KEY_CHECKS = 0; ", "TRUNCATE TABLE car_parts; ", "SET FOREIGN_KEY_CHECKS = 1; "));
-			allQueries.addAll(Arrays.asList("SET FOREIGN_KEY_CHECKS = 0; ", "TRUNCATE TABLE cars; ", "SET FOREIGN_KEY_CHECKS = 1; "));
-			allQueries.addAll(Arrays.asList("SET FOREIGN_KEY_CHECKS = 0; ", "TRUNCATE TABLE user_garage; ", "SET FOREIGN_KEY_CHECKS = 1; "));
-			allQueries.addAll(Arrays.asList("SET FOREIGN_KEY_CHECKS = 0; ", "TRUNCATE TABLE fusion_tools; ", "SET FOREIGN_KEY_CHECKS = 1; "));
-			allQueries.addAll(Arrays.asList("SET FOREIGN_KEY_CHECKS = 0; ", "TRUNCATE TABLE games; ", "SET FOREIGN_KEY_CHECKS = 1; "));
-			allQueries.addAll(Arrays.asList("SET FOREIGN_KEY_CHECKS = 0; ", "TRUNCATE TABLE garages; ", "SET FOREIGN_KEY_CHECKS = 1; "));
-			allQueries.addAll(Arrays.asList("SET FOREIGN_KEY_CHECKS = 0; ", "TRUNCATE TABLE users; ", "SET FOREIGN_KEY_CHECKS = 1; "));
 
+			allQueries.add("SET FOREIGN_KEY_CHECKS = 0;");
+			allQueries.addAll(List.of(
+					"user_order", "order_item", "orders", "user_game", "game_results",
+					"reward_item", "rewards", "user_statistics", "garage_item", "garage_car",
+					"car_parts", "cars", "user_garage", "fusion_tools", "games", "garages", "users"
+			).stream().map(table_name -> "TRUNCATE TABLE " + table_name + ";").collect(Collectors.toList()));
+			allQueries.add("SET FOREIGN_KEY_CHECKS = 1;");
 
 			for (String query : allQueries) {
 				getUserStatement.executeUpdate(query);
